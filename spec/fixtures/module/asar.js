8095056063366fd92f3066ec44c91110e26945bd
toyobayashi <lifenglin314@outlook.com>
2022-01-17 12:51:52 +0800

    test: process exit

--- spec/fixtures/module/asar.js
+++ spec/fixtures/module/asar.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
 process.on('message', function (file) {
-  process.send(fs.readFileSync(file).toString());
+  process.send(fs.readFileSync(file).toString(), (err) => {
+    process.exit(err ? 1 : 0)
+  });
 });
--- spec/fixtures/module/original-fs.js
+++ spec/fixtures/module/original-fs.js
@@ -1,3 +1,5 @@
 process.on('message', function () {
-  process.send(typeof require('original-fs'));
+  process.send(typeof require('original-fs'), (err) => {
+    process.exit(err ? 1 : 0)
+  });
 });
